7ea787ae219666d008b1e3715af51ad4f99554b0
Eric Hung <naturalsmen@gmail.com>
2017-10-10 19:44:20 +0800

    Added new object

--- src/PanelPelota.java
+++ src/PanelPelota.java
@@ -6,13 +6,14 @@ public class PanelPelota extends JPanel implements Runnable {
 
 	private static final long serialVersionUID = 1L;
 	// Positions on X and Y for the ball, player 1 and player 2
-	private int pelotaX = 10, pelotaY = 100, jug1X = 10, jug1Y = 100, jug2X = 230, jug2Y = 100;
+	private int pelotaX = 10, pelotaY = 100, pelota2X = 230, pelota2Y = 100, jug1X = 10, jug1Y = 100, jug2X = 230, jug2Y = 100;
 	Thread hilo;
-	int derecha = 5; // to the right
+	int derecha = 5;// to the right
 	int izquierda = -5; // to the left
 	int arriba = 5; // upward
 	int abajo = -5; // down
 	int ancho, alto; // Width and height of the ball
+	int ancho2, alto2;
 	// Scores
 	int contPlay1 = 0, contPlay2 = 0;
 	boolean player1FlagArr, player1FlagAba, player2FlagArr, player2FlagAba;
@@ -32,6 +33,7 @@ public class PanelPelota extends JPanel implements Runnable {
 		// Draw ball
 		gc.setColor(Color.black);
 		gc.fillOval(pelotaX, pelotaY, 8, 8);
+		gc.fillOval(pelota2X, pelota2Y, 8, 8);
 
 		// Draw ships
 		gc.fillRect(jug1X, jug1Y, 10, 25);
@@ -66,6 +68,14 @@ public class PanelPelota extends JPanel implements Runnable {
 		pelotaY = ny;
 		this.ancho = this.getWidth();
 		this.alto = this.getHeight();
+		repaint();
+	}
+	
+	public void dibujarPelota2(int nx, int ny) {
+		pelota2X = nx;
+		pelota2Y = ny;
+		this.ancho2 = this.getWidth();
+		this.alto2 = this.getHeight();
 		repaint();
 	}
 
@@ -147,6 +157,8 @@ public class PanelPelota extends JPanel implements Runnable {
 		// TODO Auto-generated method stub
 		boolean izqDer = false;
 		boolean arrAba = false;
+		boolean izqDer2 = false;
+		boolean arrAba2 = false;
 
 		while (true) {
 
@@ -179,6 +191,34 @@ public class PanelPelota extends JPanel implements Runnable {
 						arrAba = true;
 				}
 				dibujarPelota(pelotaX, pelotaY);
+				
+				// The second ball move from left to right
+				if (izqDer2) {
+					// a la derecha
+					pelota2X += derecha;
+					if (pelota2X >= (ancho - 8))
+						izqDer2 = false;
+				} else {
+					// a la izquierda
+					pelota2X += izquierda;
+					if (pelota2X <= 0)
+						izqDer2 = true;
+				}
+
+				// The ball moves from up to down
+				if (arrAba2) {
+					// hacia arriba
+					pelota2Y += arriba;
+					if (pelota2Y >= (alto - 8))
+						arrAba2 = false;
+
+				} else {
+					// hacia abajo
+					pelota2Y += abajo;
+					if (pelota2Y <= 0)
+						arrAba2 = true;
+				}
+				dibujarPelota2(pelota2X, pelota2Y);
 
 				// Delay
 				try {
@@ -194,11 +234,11 @@ public class PanelPelota extends JPanel implements Runnable {
 				moverPlayer2();
 
 				// The score of the player 1 increase
-				if (pelotaX >= (ancho - 8))
+				if (pelotaX >= (ancho - 8) || pelota2X >= (ancho2 - 8))
 					contPlay1++;
 
 				// The score of the player 2 increase
-				if (pelotaX == 0)
+				if (pelotaX == 0 || pelota2X == 0)
 					contPlay2++;
 
 				// Game over. Here you can change 6 to any value
@@ -215,8 +255,15 @@ public class PanelPelota extends JPanel implements Runnable {
 				// The ball stroke with the player 2
 				if (pelotaX == (jug2X - 5) && pelotaY >= jug2Y && pelotaY <= (jug2Y + 25))
 					izqDer = false;
+				
+				// The ball2 stroke with the player 1
+				if (pelota2X == jug1X + 10 && pelota2Y >= jug1Y && pelota2Y <= (jug1Y + 25))
+					izqDer2 = true;
+
+				// The ball2 stroke with the player 2
+				if (pelota2X == (jug2X - 5) && pelota2Y >= jug2Y && pelota2Y <= (jug2Y + 25))
+					izqDer2 = false;
 			}
 		}
 	}
-
 }
